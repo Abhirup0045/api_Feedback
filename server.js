2369771ad144c6f9370c5717009dd298c97da4ce
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-09-24 11:08:35 +0530

    config fix

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ import connctDB from './db.js'
 const app = express()
 app.use(express.json())
 
-await connctDB()
+connectDB()
+  .then(() => {
+    console.log('✅ DB connected in Vercel too')
+  })
+  .catch((err) => {
+    console.error('❌ DB connection error:', err)
+  })
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'API is Live Developer' })
