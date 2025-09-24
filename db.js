eb84f5410eb1f33fdb4852143165c395a4fa0c0b
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-09-24 11:51:35 +0530

    API fix

--- db.js
+++ db.js
@@ -2,13 +2,13 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
-dotenv.config
+dotenv.config()
 
 export default async function connectDb() {
   try {
     await mongoose.connect(process.env.DB_URI)
     console.log('connected')
   } catch (error) {
-    console.log('failed to conned to MongoDB')
+    console.log('failed to connect to MongoDB')
   }
 }
--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.use(express.json())
 
 await connectDB()
 
+app.post('/users', async (req, res) => {
+  const data = await Feedback.create(req.body)
+  res.status(201).json(data)
+})
+
 app.get('/users', async (req, res) => {
   const data = await Feedback.find({})
   res.status(201).json(data)
@@ -14,5 +19,10 @@ app.get('/users', async (req, res) => {
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'API is Live Developer' })
 })
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => console.log(`🚀 Local server running on port ${PORT}`))
+}
 
 export default app
