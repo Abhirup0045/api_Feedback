af428988d5f717597af16edde3444ba3b55a136c
Abhirup0045 <mentalrockeyroy@gamil.com>
2025-09-25 00:49:26 +0530

    added CORS

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import connectDB from '../db.js'
 import Feedback from '../models/schema.js'
+import cors from 'cors'
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.post('/api/users', async (req, res) => {
